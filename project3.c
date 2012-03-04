cefd80d7c2ec58fe31be1a7500f030759d30f646
Alexander Hall: Work Mac <Alexander.d.hall@gmail.com>
2012-03-04 17:37:30 -0500

    Added cmdfile, with example commands. Added project 3 author info

--- project3.c
+++ project3.c
@@ -1,1 +1,25 @@
-// Alex Hall
+/*****************************************
+ ** File: project3.c
+ ** Author: Alexander D. Hall
+ ** Date: 03/04/12
+ ** Section: cs313-02 spring12
+ ** E-mail: hall9@umbc.edu
+ **
+ ** This file contails the main driver program for project 3.
+ ** This program read the file specified and reads each command in the file followed
+ ** the command arugments.
+ **
+ **
+ ** Other Files requierd are
+ ** 1. cmdFile - This file contains the commands and arugments for the program to preform
+ **
+ ***********************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (char filename[] ) {
+    
+    
+    
+}
